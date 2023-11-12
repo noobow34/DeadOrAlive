9762f6653b22654ab107408c688e68995b97fd95
noobow34 <skywind.hk34@gmail.com>
2023-11-12 09:22:32 +0900

    api v2対応 リファクタリング 不具合修正

--- DeadOrAlive/Function.cs
+++ DeadOrAlive/Function.cs
@@ -7,7 +7,7 @@ using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
-using CoreTweet;
+using SocialOpinionAPI.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -16,9 +16,8 @@ namespace DeadOrAlive
 {
     public class Function
     {
-        private static readonly AmazonDynamoDBClient dbClient = new AmazonDynamoDBClient(RegionEndpoint.APNortheast1);
-        private static readonly HttpClient httpClient = new HttpClient();
-        private Tokens tokens = null;
+        private static readonly AmazonDynamoDBClient dbClient = new(RegionEndpoint.APNortheast1);
+        private static readonly HttpClient httpClient = new();
 
         /// <summary>
         /// 
@@ -27,38 +26,43 @@ namespace DeadOrAlive
         /// <returns></returns>
         public async Task FunctionHandlerAsync(ILambdaContext context)
         {
-
-            using (var dbContext = new DynamoDBContext(dbClient))
+            using var dbContext = new DynamoDBContext(dbClient);
+            var monitorTargets = await dbContext.ScanAsync<DaMonitor>(new List<ScanCondition>()).GetRemainingAsync();
+            var batchWrite = dbContext.CreateBatchWrite<DaMonitor>();
+            foreach (var monitor in monitorTargets)
             {
-                var monitorTargets = await dbContext.ScanAsync<DaMonitor>(new List<ScanCondition>()).GetRemainingAsync();
-                var batchWrite = dbContext.CreateBatchWrite<DaMonitor>();
-                foreach (var monitor in monitorTargets)
+                context.Logger.LogLine($"{monitor.Url}:{monitor.Status}:{monitor.ChamgeTime}");
+                HttpResponseMessage response = null;
+                try
                 {
-                    context.Logger.LogLine($"{monitor.Url}:{monitor.Status}:{monitor.ChamgeTime}");
-                    var response = await httpClient.GetAsync(monitor.Url, HttpCompletionOption.ResponseHeadersRead);
+                    response = await httpClient.GetAsync(monitor.Url, HttpCompletionOption.ResponseHeadersRead);
                     context.Logger.LogLine($"Status:{response.StatusCode}");
-
-                    if(response.StatusCode != System.Net.HttpStatusCode.OK && monitor.Status == 0)
-                    {
-                        //今回からNGになった
-                        monitor.Status = 1;
-                        monitor.ChamgeTime = DateTime.Now.ToString();
-                        batchWrite.AddPutItem(monitor);
-                        await PushLineNotifyAsync($"【ダウン検知】\n{monitor.ServiceName}\n{monitor.Url}\n{DateTime.Now.ToString()}");
-                        DoTweet($"【自動ツイート】{Environment.NewLine}現在{monitor.ServiceName}が停止しています。復旧までしばらくお待ち下さい。{Environment.NewLine}{DateTime.Now.ToString()}");
-                    }
-                    if (response.StatusCode == System.Net.HttpStatusCode.OK && monitor.Status != 0)
-                    {
-                        //今回からOKになった
-                        monitor.Status = 0;
-                        monitor.ChamgeTime = DateTime.Now.ToString();
-                        batchWrite.AddPutItem(monitor);
-                        await PushLineNotifyAsync($"【アップ検知】\n{monitor.Url}\n{monitor.ServiceName}\n{DateTime.Now.ToString()}");
-                        DoTweet($"【自動ツイート】{Environment.NewLine}現在{monitor.ServiceName}が復旧しました。{Environment.NewLine}{DateTime.Now.ToString()}");
-                    }
                 }
-                await batchWrite.ExecuteAsync();
+                catch
+                {
+                    context.Logger.LogLine($"Status:exception");
+                }
+
+                if ((response == null || response.StatusCode != System.Net.HttpStatusCode.OK) && monitor.Status == 0)
+                {
+                    //今回からNGになった
+                    monitor.Status = 1;
+                    monitor.ChamgeTime = DateTime.Now.ToString();
+                    batchWrite.AddPutItem(monitor);
+                    await PushLineNotifyAsync($"【ダウン検知】\n{monitor.ServiceName}\n{monitor.Url}\n{DateTime.Now}");
+                    DoTweet($"【自動ツイート】{Environment.NewLine}現在{monitor.ServiceName}が停止しています。復旧までしばらくお待ち下さい。{Environment.NewLine}{DateTime.Now}");
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.OK && monitor.Status != 0)
+                {
+                    //今回からOKになった
+                    monitor.Status = 0;
+                    monitor.ChamgeTime = DateTime.Now.ToString();
+                    batchWrite.AddPutItem(monitor);
+                    await PushLineNotifyAsync($"【アップ検知】\n{monitor.Url}\n{monitor.ServiceName}\n{DateTime.Now}");
+                    DoTweet($"【自動ツイート】{Environment.NewLine}現在{monitor.ServiceName}が復旧しました。{Environment.NewLine}{DateTime.Now}");
+                }
             }
+            await batchWrite.ExecuteAsync();
         }
 
         /// <summary>
@@ -74,7 +78,7 @@ namespace DeadOrAlive
             });
 
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Environment.GetEnvironmentVariable("LINE_TOKEN"));
-            var result = await httpClient.PostAsync("https://notify-api.line.me/api/notify", content);
+            _ = await httpClient.PostAsync("https://notify-api.line.me/api/notify", content);
         }
 
         /// <summary>
@@ -83,15 +87,15 @@ namespace DeadOrAlive
         /// <param name="message"></param>
         private void DoTweet(string message)
         {
-            if(tokens == null)
+            var client = new SocialOpinionAPI.Clients.TweetsClient(new OAuthInfo
             {
-                tokens = Tokens.Create(Environment.GetEnvironmentVariable("TWITTER_API_KEY")
-                                           , Environment.GetEnvironmentVariable("TWITTER_API_SECRET")
-                                           , Environment.GetEnvironmentVariable("TWITTER_ACCESS_KEY")
-                                           , Environment.GetEnvironmentVariable("TWITTER_ACCESS_SECRET"));
-            }
+                AccessToken = Environment.GetEnvironmentVariable("TWITTER_ACCESS_TOKEN"),
+                AccessSecret = Environment.GetEnvironmentVariable("TWITTER_ACCESS_SECRET"),
+                ConsumerKey = Environment.GetEnvironmentVariable("TWITTER_CONSUMER_KEY"),
+                ConsumerSecret = Environment.GetEnvironmentVariable("TWITTER_CONSUMER_SECRET")
+            });
 
-            tokens.Statuses.Update(status => message);
+            client.PostTweet(message);
         }
     }
 }
